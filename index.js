30338f992b615d0979dc9af238656086872d7c81
jensjoris <jensjoris@techwolf.ai>
2020-11-16 17:39:42 +0100

    Voorbeeldoplossing

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ db.serialize(() => {
     db.run(
       "CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);"
     );
-    console.log("Database created");
+    db.run(
+      "CREATE TABLE Notes (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT, userId INTEGER, FOREIGN KEY(userId) REFERENCES Users(id));"
+    );
   }
 });
 
@@ -42,6 +44,149 @@ app.get("/users", (req, res) => {
       return;
     }
     res.send(JSON.stringify(rows));
+  });
+});
+
+// Voeg een gebruiker toe aan de database
+app.post("/users", (req, res) => {
+  // /users url in plaats van /add (1 URL kan meerdere methodes beantwoorden)
+  // controleer of de name parameter is meegegeven
+  let name = req.body.name; // POST data zit in de HTTP body (en dus niet in de url parameters)
+  if (!name) {
+    res.send({ error: "No name argument found" });
+    return;
+  }
+
+  db.all(`SELECT name from Users WHERE name Like (?)`, name, (err, rows) => {
+    // return eventuele errors
+    if (err) {
+      res.send({ error: err });
+      return;
+    }
+    if (rows.length > 0) {
+      res.send({ error: "User already exists" });
+      return;
+    }
+
+    db.run(`INSERT INTO Users (name) VALUES (?)`, name, (error) => {
+      if (error) {
+        res.send({ error: error });
+        return;
+      }
+      res.send({ success: `Successfully added ${name}` });
+    });
+  });
+});
+
+// Voeg een notitie toe
+app.get("/addnote", (req, res) => {
+  // controleer of de parameters meegegeven zijn
+  let name = req.query.name;
+  let content = req.query.content;
+  if (!name || !content) {
+    res.send({ error: "Parameters name and content are required" });
+    return;
+  }
+
+  // haal de gebruiker op uit de database
+  db.get(`SELECT id FROM Users WHERE name LIKE ?`, [name], (err, row) => {
+    // return een eventuele error
+    if (err) {
+      res.send({ error: err });
+      return;
+    }
+    if (!row) {
+      res.send({ error: `No user named ${name}` });
+      return;
+    }
+
+    // haal alle notities van de gebruiker op
+    let userId = row.id;
+    db.run(
+      `INSERT INTO Notes(content, userId) VALUES(?, ?)`,
+      [content, userId],
+      (err) => {
+        if (err) {
+          res.send({ error: err });
+          return;
+        }
+        res.send({ success: "Inserted note" });
+      }
+    );
+  });
+});
+
+// Antwoord met een lijst van alle notities van een gebruiker
+app.get("/notes", (req, res) => {
+  // controleer of de naam parameter is meegegeven
+  let name = req.query.name;
+  if (!name) {
+    res.send({ error: "No name argument found" });
+    return;
+  }
+
+  // haal de userId van de gebruiker op
+  db.get(`SELECT id from Users WHERE name LIKE (?)`, name, (err, row) => {
+    // return bij eventuele error
+    if (err) {
+      res.send({ error: err });
+      return;
+    }
+    // controleer of een gebruiker met de naam bestaat
+    if (!row) {
+      res.send({ error: `No user named ${name}` });
+      return;
+    }
+
+    // haal alle notities van de gebruiker op
+    let userId = row.id;
+    db.all("SELECT * from Notes WHERE userId = ?", userId, (err, rows) => {
+      if (err) {
+        res.send({ error: err });
+        return;
+      }
+      res.send(JSON.stringify(rows));
+    });
+  });
+});
+
+// Verwijdert gebruiker met diens notities
+app.get("/remove", (req, res) => {
+  // controleer of de naam parameter is meegegeven
+  let name = req.query.name;
+  if (!name) {
+    res.send({ error: "No name argument found" });
+    return;
+  }
+
+  db.get(`SELECT id FROM Users WHERE name LIKE ?`, [name], (err, row) => {
+    // return bij eventuele error
+    if (err) {
+      res.send({ error: err });
+      return;
+    }
+    // controleer of een gebruiker met de naam bestaat
+    if (!row) {
+      res.send({ error: `No user named ${name}` });
+      return;
+    }
+
+    // verwijder notities van de gebruiker
+    let userId = row.id;
+    db.run(`DELETE FROM Notes WHERE userId = (?)`, [userId], (err) => {
+      if (err) {
+        res.send({ error: err });
+        return;
+      }
+    });
+    // verwijder de gebruiker
+    db.run(`DELETE FROM Users WHERE id = (?)`, [userId], (err) => {
+      if (err) {
+        res.send({ error: err });
+        return;
+      }
+      res.send({ success: "Deleted successfully" });
+    });
   });
 });
 
